3ea4f8fa36f996b690c65068289c585e24f6c885
Tobias Nagel <tobias.nagel@mintellity.com>
2023-01-18 21:28:58 +0000

    Add attributes passthrough to all buttons

--- resources/views/components/yellow.blade.php
+++ resources/views/components/yellow.blade.php
@@ -1,3 +1,3 @@
-<a href="{{ ($route ?? false) ? route($route) : ($link ?? '#') }}" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">
+<a {{ $attributes }} href="{{ ($route ?? false) ? route($route) : ($link ?? '#') }}" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">
     {{ $text }}
 </a>
